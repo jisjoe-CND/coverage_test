173e051fe6a7ce153d50bab0a03cc8875f470dde
jisjoe-CND <jisjoe@carnextdoor.com.au>
2023-04-24 13:09:30 +0530

    add tests

--- lib/helper.dart
+++ lib/helper.dart
@@ -6,4 +6,12 @@ class Helper {
   static num subtract(a, b) {
     return a - b;
   }
+
+  bool isPalindrome(String str) {
+    // Convert the string to lowercase and remove non-alphanumeric characters
+    String cleanStr = str.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), '');
+
+    // Compare the clean string to its reverse
+    return cleanStr == cleanStr.split('').reversed.join('');
+  }
 }
