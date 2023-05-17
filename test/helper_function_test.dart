c606ccbb6c4db78c0979696753ec13b87c2f95a5
Jis Joe Mathew <85979180+jisjoe-CND@users.noreply.github.com>
2023-05-17 11:05:36 +0530

    Add capitalize helper funciton (#5)
    * add capitalize helper funciton
    
    * add lcov

--- test/helper_function_test.dart
+++ test/helper_function_test.dart
@@ -1,7 +1,5 @@
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test_coverage/helper.dart';
-
 
 void main() {
   test(
@@ -17,4 +15,20 @@ void main() {
       expect(Helper.subtract(2, 3), -1);
     },
   );
+
+  group('capitalize', () {
+    test('capitalizes the first letter of each word', () {
+      String input = 'the quick brown fox jumps over the lazy dog';
+      String expectedOutput = 'The Quick Brown Fox Jumps Over The Lazy Dog';
+      String result = Helper.capitalize(input);
+      expect(result, expectedOutput);
+    });
+
+    test('handles empty input', () {
+      String input = '';
+      String expectedOutput = '';
+      String result = Helper.capitalize(input);
+      expect(result, expectedOutput);
+    });
+  });
 }
